411ee037c40c428ac27bcfd61cddba623329c725
Andrew Martins <andrew_omartins@yahoo.com>
2021-05-02 16:22:46 -0400

    first commit

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,20 +1,15 @@
-import { useParams} from "react-router-dom";
-import { useState, useEffect } from 'react';
-
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const UserProfile = (props) => {
-    const { username } = useParams();
-    const [repoList, setRepoList] = useState([]);
-    useEffect(() => {
-        (async function() {
-            const url = `https://api.guthub.com/users.${username}/repos`;
-            const userInfo = await fetch(url)
-                .then(reponse => reponse.json())
-                console.log("USERNAME: ", userInfo);
-                setRepoList(userInfo);
-        })
-    })
-    
-
-}
+  const { username } = useParams();
+  const [repoList, setRepoList] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const url = `https://api.guthub.com/users.${username}/repos`;
+      const userInfo = await fetch(url).then((reponse) => reponse.json());
+      console.log("USERNAME: ", userInfo);
+      setRepoList(userInfo);
+    });
+  });
+};
